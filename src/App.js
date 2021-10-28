457ee5396f8b5b3f46c91728926f02bc2568bc80
ula345 <krzeminska.ula@gmail.com>
2021-10-28 20:56:31 +0200

    Search engine created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
 import logo from "./logo.jpg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
  function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
     </header>
+    <main> 
+      <Dictionary />
+      </main> 
+    <footer className="text-center">
+       <small> Coded by Urszula Krzemińska and is {" "}
+          <a href ="https://github.com/ula345/weather-app-react.git" rel="noreferrer" target="_blank"> open-sourrce on GitHub </a>
+    and is run by <a href ="https://condescending-euler-9266ff.netlify.app" rel="noreferrer" target="_blank">Netlify</a>
+    </small>
+    </footer>
+    </div>
     </div>
   );
 }
