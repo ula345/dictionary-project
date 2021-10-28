a329219d250761e24ea35c7d6ed5c1180ece8643
ula345 <krzeminska.ula@gmail.com>
2021-10-28 22:07:59 +0200

    meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,22 @@
 import React, {useState} from "react"; 
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) { 
 console.log(response.data[0]);
+setResults(response.data[0]);
+console.log(response.data[0].meanings[0].definitions[0].definition);
     }
 
 function search(event) { 
     event.preventDefault();
     
-
 let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);}
 
@@ -26,6 +29,8 @@ return (
 <form onSubmit={search}> 
     <input type="search" onChange={handleKeywordChange} /> 
     </form>
+
+    <Results results={results}/>
     </div>);
    
 }
