e88c4faddb72f03db510975b4ea4fa3329061f6e
ula345 <krzeminska.ula@gmail.com>
2021-10-30 21:57:53 +0200

    a href updated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,12 +11,22 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) { 
+
+
+  }
   function search(event) {
     event.preventDefault();
 
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey="563492ad6f9170000100000141dbf2b031c344e3aa299ac026140493";
+    let pexelsApiUrl=`https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        axios.get(pexelsapiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`},
+  }).then(handlePexelsResponse);
+
   }
 
   function handleKeywordChange(event) {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
 
 return (
     <div class="Phonetic"> 
-    < a href={props.phonetic.audio} target="_blank">Listen</a>
+    < a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
 <span className="text">
     {props.phonetic.text} </span> 
     </div>
